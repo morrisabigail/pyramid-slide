9e38dc796967231f85cb6b5bf351f6ac8bed020e
Abi Morris <Morrisabigail@icloud.com>
2017-03-16 18:49:35 -0500

    add dynamic dropdown for brick selection

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -1,21 +1,28 @@
-height = document.getElementById("height").value
-window.onload = drawPyramid(height);
-function updateSlider(height){
-   document.querySelector('#height').value = height;
-   drawPyramid(height);
+var height = document.getElementById("height").value;
+var brick = document.getElementById("selection").value;
+
+function getBrick(brick){
+  document.querySelector("#selection").value = brick;
+  console.log(brick);
+  drawPyramid(height,brick);
 }
-// document.querySelector('#height').addEventListener('oninput', function(){
-//     height = drawPyramid(height);
-//
-// });
+window.onload = drawPyramid(height,brick);
+//var select = document.getElementById("select").value;
 
+function updateSlider(height){
+  var brick = document.querySelector("#selection").value;
+  document.querySelector('#height').value = height;
+  drawPyramid(height,brick);
+}
 
-function drawPyramid(height) {
+// function getBrick(selection){
+//   document.querySelector("#selection").value = brick;
+//   console.log(brick);
+// }
+function drawPyramid(height,brick) {
     // for each row....
-  //  document.getElementById("#pyramid").innerHTML='';
-    document.getElementById("pyramid").innerHTML = '';
+    document.getElementById("pyramid").innerHTML = "";
     for (var row = 0; row < height; row++) {
-
         // figure out number of bricks and spaces
         var numBricks = row + 2;
         var numSpaces = height - row - 1;
@@ -23,10 +30,11 @@ function drawPyramid(height) {
         // build up a string for this row
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
-            rowStr += ".";
+            var spaceChar = "&nbsp;"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
         }
         for (var i = 0; i < numBricks; i++) {
-            rowStr += "#";
+            rowStr += brick;
         }
 
        // create a text element with the string of characters
@@ -34,7 +42,7 @@ function drawPyramid(height) {
 
        // create a <p> element with the text inside
        rowElem = document.createElement("p");
-       rowElem.appendChild(textElem);
+       rowElem.innerHTML = rowStr;
 
        // insert the paragraph as a child of the container <div>
        document.getElementById("pyramid").appendChild(rowElem);
